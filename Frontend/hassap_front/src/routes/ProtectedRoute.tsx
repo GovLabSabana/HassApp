19fd7921af13cab23b7baff315d9fdef1701425b
Jhojan Jimenez <jhojanjimene@gmail.com>
2025-06-03 18:25:46 -0500

    Change Package.json

--- Frontend/hassap_front/src/routes/ProtectedRoute.tsx
+++ Frontend/hassap_front/src/routes/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
-import { Outlet, Navigate } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function ProtectedRoute() {
-    const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
 
-    return isAuth ? <Outlet /> : <Navigate to="/" />
+  return isAuth ? <Outlet /> : <Navigate to="/" />;
 }
