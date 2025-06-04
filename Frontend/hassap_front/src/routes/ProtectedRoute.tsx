4613c9c86a13285f981b808a11b46c0594368cd4
Juan Jose R <15juanjo2004@gmail.com>
2025-06-04 11:52:57 -0500

    verificacion_accessToken

--- Frontend/hassap_front/src/routes/ProtectedRoute.tsx
+++ Frontend/hassap_front/src/routes/ProtectedRoute.tsx
@@ -1,8 +1,19 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function ProtectedRoute() {
-  const token = localStorage.getItem('access_token');
-  const isAuth = !!token; // Convertir a booleano
+  const [isAuth, setIsAuth] = useState(!!localStorage.getItem('access_token'));
+
+  useEffect(() => {
+    const checkToken = () => {
+      const token = localStorage.getItem('access_token');
+      setIsAuth(!!token);
+    };
+
+    const intervalId = setInterval(checkToken, 1000); // Verifica cada segundo
+
+    return () => clearInterval(intervalId); // Limpieza
+  }, []);
 
   return isAuth ? <Outlet /> : <Navigate to="/" />;
 }
