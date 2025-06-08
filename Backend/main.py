ebd5aa2579a1af7fe2ad983cb0f9c591e0ac6a7d
Jhojan Jimenez <jhojanjimene@gmail.com>
2025-06-07 19:08:42 -0500

    Solve deploy problems in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -3,15 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 from pydantic import ValidationError
-<<<<<<< HEAD
-from api.fake_data import router as fake_router
-
-
-from api import auth, protegida, predio, usuario, cosecha, comprador, exportaciones,reset_data
-=======
 import os
 from api import auth, protegida, predio, usuario, cosecha, comprador, exportaciones
->>>>>>> cf17e03eb82cc4fab702e22d54619757d8e47775
 
 load_dotenv()
 
@@ -26,7 +19,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# Routers
+# Routerss
 app.include_router(usuario.router)
 app.include_router(auth.router)
 app.include_router(predio.router)
@@ -34,8 +27,6 @@ app.include_router(cosecha.router)
 app.include_router(protegida.router, prefix="/api")  # Rutas protegidas
 app.include_router(comprador.router)
 app.include_router(exportaciones.router)
-app.include_router(reset_data.router)
-app.include_router(fake_router)
 
 
 # Manejo de errores de validación
