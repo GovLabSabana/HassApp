df8b0bca490cad0d283bc79a8a97b25b4b65772c
Jhojan Jimenez <jhojanjimene@gmail.com>
2025-06-07 20:05:18 -0500

    Solve logo_document in create Usuario

--- Backend/core/s3.py
+++ Backend/core/s3.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import mimetypes
 from botocore.exceptions import NoCredentialsError
 from dotenv import load_dotenv
 from urllib.parse import quote, urlsplit, urlunsplit
@@ -22,12 +23,17 @@ s3_client = session.client('s3')
 
 def upload_file_to_s3(file_obj, filename):
     try:
+        content_type, _ = mimetypes.guess_type(filename)
+        if content_type is None:
+            content_type = 'application/octet-stream'  # fallback por si no se detecta
+
         s3_client.upload_fileobj(
             Fileobj=file_obj,
             Bucket=AWS_S3_BUCKET_NAME,
             Key=filename,
-            ExtraArgs={'ContentType': 'application/pdf',
-                       'ContentDisposition': 'inline'}
+            ExtraArgs={
+                'ContentType': content_type,
+            }
         )
         url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_REGION}.amazonaws.com/{filename}"
         return url_encode_s3_object_url(url)
@@ -36,7 +42,6 @@ def upload_file_to_s3(file_obj, filename):
 
 
 def url_encode_s3_object_url(url: str) -> str:
-
     parts = urlsplit(url)
     path = quote(parts.path)
     return urlunsplit((parts.scheme, parts.netloc, path, parts.query, parts.fragment))
