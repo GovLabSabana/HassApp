e0c384d570e63c5bb15904d7781a4f6c374bdd0a
Jhojan Jimenez <jhojanjimene@gmail.com>
2025-06-19 20:58:23 -0500

    Solve problems in Exportaciones, Insumos, Cosechas

--- Backend/models/insumo_cosecha.py
+++ Backend/models/insumo_cosecha.py
@@ -2,14 +2,19 @@ from sqlalchemy import Column, Integer, ForeignKey, DECIMAL
 from sqlalchemy.orm import relationship
 from core.db import Base
 from models.insumo import Insumo
-from decimal import Decimal        
+from decimal import Decimal
 
 
 class InsumoCosecha(Base):
     __tablename__ = "insumo_cosecha"
 
     id = Column(Integer, primary_key=True, index=True)
-    insumo_id = Column(Integer, ForeignKey("insumo.id"), nullable=False)
+    insumo_id = Column(
+        Integer,
+        ForeignKey("insumo.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
     cosecha_id = Column(Integer, ForeignKey("cosecha.id"), nullable=False)
     cantidad = Column(DECIMAL(10, 2), nullable=False)
 
--- Backend/schemas/exportaciones.py
+++ Backend/schemas/exportaciones.py
@@ -1,3 +1,4 @@
+from optparse import Option
 from typing import List, Optional
 from pydantic import BaseModel
 from pydantic.config import ConfigDict
@@ -14,19 +15,28 @@ class ExportacionBase(BaseModel):
     puerto_llegada: Optional[str]
     comprador_id: int
 
-class ExportacionCreate(ExportacionBase):
-    cosecha_ids: List[int]  #  para crear con relaciones
 
-class ExportacionUpdate(ExportacionBase):
-    cosecha_ids: Optional[List[int]] = None  # para actualizar relaciones
+class ExportacionCreate(ExportacionBase):
+    cosecha_ids: List[int]  # para crear con relaciones
+
+
+class ExportacionUpdate(BaseModel):
+    fecha: Optional[date] = None
+    metodo_salida: Optional[str] = None
+    toneladas: Optional[float] = None
+    valor_fob: Optional[float] = None
+    puerto_salida: Optional[str] = None
+    puerto_llegada: Optional[str] = None
+    comprador_id: Optional[int] = None
+    cosecha_ids: Optional[List[int]] = None
 
 
 class ExportacionOut(ExportacionBase):
     id: int
     cosecha_ids: Optional[List[int]] = []
 
-
     model_config = ConfigDict(from_attributes=True)
+
 
 class ExportacionOutWithCosechas(ExportacionOut):
     cosechas: Optional[List[CosechaRead]] = []
