cb6fdb8b419b91261817b493385a6739ca541c3d
Jhojan Jimenez <jhojanjimene@gmail.com>
2025-06-25 17:25:48 -0500

    Requeriments

--- Backend/tests/auth_test.py
+++ Backend/tests/auth_test.py
@@ -5,10 +5,6 @@ import pytest
 from httpx import AsyncClient
 from main import app
 
-
-load_dotenv()
-
-# Cargar específicamente el archivo .env.test
 load_dotenv(dotenv_path=".env.test")
 
 BACK_URL = os.getenv('BACKEND_URL'),
@@ -16,7 +12,7 @@ BACK_URL = os.getenv('BACKEND_URL'),
 
 @pytest.mark.asyncio
 async def test_login_correcto():
-    client = TestClient(app)  # Este sí acepta `app`
+    client = TestClient(app)
     response = client.post(
         "/auth/jwt/login",
         data={"username": "da@gmail.com", "password": "123"},
