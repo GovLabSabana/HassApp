c35ed688e4b87b3f85b65ac15eb9aa003e9419d6
Santii08 <dan.santiago.rc2005@gmail.com>
2025-07-01 20:15:12 -0500

    cambios y adaptacioens certifiacioens_predio

--- Backend/api/certificaciones_predio.py
+++ Backend/api/certificaciones_predio.py
@@ -17,6 +17,8 @@ from typing import Optional
 from sqlalchemy.orm import selectinload
 from core.s3 import upload_file_to_s3, delete_file_from_s3
 from urllib.parse import urlparse
+from fastapi import Query
+
 
 
 import uuid
@@ -59,6 +61,27 @@ async def crear_certificacion_predio(
     await db.refresh(nueva)
     return nueva
 
+@router.get("/", response_model=list[CertificacionPredioRead])
+async def filtrar_certificaciones_predio(
+    predio_id: Optional[int] = Query(None),
+    certificacion_id: Optional[int] = Query(None),
+    db: AsyncSession = Depends(get_db),
+    user: Usuario = Depends(current_user)
+):
+    query = (
+        select(CertificacionPredio)
+        .join(Predio)
+        .options(selectinload(CertificacionPredio.certificacion))
+        .where(Predio.usuario_id == user.id)
+    )
+
+    if predio_id is not None:
+        query = query.where(CertificacionPredio.predio_id == predio_id)
+    if certificacion_id is not None:
+        query = query.where(CertificacionPredio.certificacion_id == certificacion_id)
+
+    result = await db.execute(query)
+    return result.scalars().all() 
 
 @router.get("/{predio_id}", response_model=list[CertificacionPredioRead])
 async def listar_certificaciones_de_predio(
